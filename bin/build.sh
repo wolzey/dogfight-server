ae4c621607fdc56f926c0b5297a5ab99741a37a9
Ethan Wolz <ethan@creadiv.io>
2018-01-19 14:32:58 -0800

    Why

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -rf dist
+rm -rf dist.zip
 mkdir dist
 TARGETDIR='dist';for file in *;do test "$file" != "$TARGETDIR" && cp -r "$file" "$TARGETDIR/";done
+zip -rq dist.zip dist/
 rm -rf dist/node_modules
