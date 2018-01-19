ceeee3108caa6e6d442815c7a33362f65a340a63
Ethan Wolz <ethan@creadiv.io>
2018-01-18 20:13:30 -0800

    doing the most insecure thing ever

--- bin/prep-environment.sh
+++ bin/prep-environment.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm -rf /var/www/html/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
 echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+
+sudo su root
+rm -rf /var/www/html
