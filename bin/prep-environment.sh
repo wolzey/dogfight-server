4ef4e72cc6103aa9c41da5645c02031e76158e56
Ethan Wolz <ethan@creadiv.io>
2018-01-18 20:11:21 -0800

    before install

--- bin/prep-environment.sh
+++ bin/prep-environment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf /var/www/html/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
