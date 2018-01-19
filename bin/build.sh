581c37e14ad0b673d1c13188d108bee419c1e2e1
Ethan Wolz <ethan@creadiv.io>
2018-01-19 13:56:39 -0800

    why

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf dist
 mkdir dist
-TARGETDIR='dist';for file in *;do test [[ "$file" != "$TARGETDIR" && "$file" != 'node_modules']] && cp -r "$file" "$TARGETDIR/";done
+TARGETDIR='dist';for file in *;do test ([[ "$file" != "$TARGETDIR" && "$file" != 'node_modules']]) && cp -r "$file" "$TARGETDIR/";done
