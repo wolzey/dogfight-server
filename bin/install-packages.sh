5c5a62b1d14160ac998a2b95d3e5d2b62cc90dd1
Ethan Wolz <ethan@creadiv.io>
2018-01-18 20:14:31 -0800

    Amazon can suck a dick

--- bin/install-packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-brew install node
-npm install -g pm2
-npm install
--- bin/prep-environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
-echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
-
-sudo su root
-rm -rf /var/www/html
