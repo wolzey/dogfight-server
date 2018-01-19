18fc6c9d6e7833c8c6f681a1cab9fadae2740ab5
Ethan Wolz <ethan@creadiv.io>
2018-01-19 13:58:36 -0800

    GRR

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -rf dist
 mkdir dist
-TARGETDIR='dist';for file in *;do test ([[ "$file" != "$TARGETDIR" && "$file" != 'node_modules']]) && cp -r "$file" "$TARGETDIR/";done
+TARGETDIR='dist';for file in *;do test "$file" != "$TARGETDIR" && "$file" && cp -r "$file" "$TARGETDIR/";done
+cd dist
+rm -rf node_modules
